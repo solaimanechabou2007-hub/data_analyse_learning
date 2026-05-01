7f23bf84f785aabede15daeeff0ae040254e8d44
solaimanechabou2007-hub <solaimanechabou2007@gmail.com>
2026-05-01 14:58:24 +0200

    Add: students groupby analysis

--- 01_pandas/DataFram.py
+++ 01_pandas/DataFram.py
@@ -1,20 +1,39 @@
-#stor_sales_analysis.py 
+# Students GroupBy Analysis
 import pandas as pd 
 data = {
-    "product": ("phone", "tv", "computer"),
-    "quantity": (5000, 1000, 3000),
-    "prix": (1000, 3000, 2000)
+    "name": ("solaimane", "amine", "mohamed"),
+    "note": (13, 16, 17),
+    "city": ("mknes", "imilchile", "marakech",),
+    "departement": ("phisique", "francis", "mathematiques")
 }
 df = pd.DataFrame(data)
-#add total column 
-df["total"] = df["quantity"] * df["prix"]
-print(df["total"])
-#total sales 
-print(df["total"].sum())
-#the most expencive product
-print(df.loc[df["prix"].idxmax(), "product"])
-#total > 5000 
-print(df[df["total"] > 5000 ])
+#calculat th average score for ech city with groupby
+df.groupby("city")["note"].mean()
+#average for any city
+df["average"] = df["note"].mean()
+print(df["average"]) 
+#Add the results column if the point is greater than or equal to 10 it is successful
+#, and if the word is less, it is a sedipitate
+df["results"] = df["note"].apply(lambda x: "passé" if x >= 10 else "failed" )
+#print the number of succceful and repeaters
+print(df[df["results"] == "passé"].shape[0])
+print(df[df["results"] == "failed"].shape[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
